b0f91ee9276d7d760f8c6e7549c0338c4dc45922
kek1488 <74298038+glebkhaletsky@users.noreply.github.com>
2021-04-11 16:15:25 +0300

    update
    added the last enemy character and its interaction

---
+++ Assets/Scripts/RotateToTargetEuler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToTargetEuler : MonoBehaviour
+{
+    public Vector3 LeftTarget;
+    public Vector3 RightTarget;
+    private Vector3 _toTarget;
+
+    public float RotationSpeed;
+
+    private void Update()
+    {
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(_toTarget), Time.deltaTime * RotationSpeed);
+    }
+
+    public void ToLeft()
+    {
+        _toTarget = LeftTarget;
+    }
+    public void ToRight()
+    {
+        _toTarget = RightTarget;
+    }
+}
---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum Direction
+{
+    Left,
+    Right
+}
+public class Walker : MonoBehaviour
+{
+    public Transform LeftPoint;
+    public Transform RightPoint;
+
+    public float Speed;
+    public float StopTime;
+
+    public Direction CurrentDirection;
+
+    private bool _isStoped;
+
+    public UnityEvent EventOnLeftTarget;
+    public UnityEvent EventOnRightTarget;
+
+    public Transform RayStart;
+
+    private void Start()
+    {
+        LeftPoint.parent = null;
+        RightPoint.parent = null;
+    }
+
+    private void Update()
+    {
+        if (_isStoped == true)
+        {
+            return;
+        }
+
+        if(CurrentDirection == Direction.Left)
+        {
+            transform.position -= new Vector3(Time.deltaTime * Speed, 0, 0);
+            if (transform.position.x < LeftPoint.position.x)
+            {
+                CurrentDirection = Direction.Right;
+                _isStoped = true;
+                Invoke("ContinueWalk", StopTime);
+                EventOnLeftTarget.Invoke();
+            }
+        }
+        else
+        {
+            transform.position += new Vector3(Time.deltaTime * Speed, 0, 0);
+            if (transform.position.x > RightPoint.position.x)
+            {
+                CurrentDirection = Direction.Left;
+                _isStoped = true;
+                Invoke("ContinueWalk", StopTime);
+                EventOnRightTarget.Invoke();
+            }
+        }
+
+        RaycastHit hit;
+        if(Physics.Raycast(RayStart.position,Vector3.down,out hit))
+        {
+            transform.position = hit.point;
+        }
+    }
+
+    void ContinueWalk()
+    {
+        _isStoped = false;
+    }
+}
